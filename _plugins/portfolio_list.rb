3dbb031e4719b4e6f6a87f577a78c401d72607ae
Hez Ronningen <code@hez.ca>
2011-11-18 12:48:21 -0800

    helper for generating lists of images

---
+++ _plugins/portfolio_list.rb
@@ -0,0 +1,17 @@
+require 'liquid'
+
+# Percent encoding for URI conforming to RFC 3986.
+# Ref: http://tools.ietf.org/html/rfc3986#page-12
+BASE_DIR = 'images/portfolio'
+module PortfolioList
+  def portfolio_image_list( name )
+    list = Array.new
+    dir = Dir.new( File.join(BASE_DIR, name) )
+    dir.each do | d |
+      list << "'/#{BASE_DIR}/#{name}/#{d}'" unless d =~ /^\./
+    end
+    list.join( ',' )
+  end
+end
+
+Liquid::Template.register_filter(PortfolioList)
